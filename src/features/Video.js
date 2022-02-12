968e45d3c04af53bd58c69cf37a292f3435c4ee4
k.chistyakov <k.chistyakov@city-mobil.ru>
2022-02-12 10:50:01 +0300

    some changes in Video

--- src/features/Video.js
+++ src/features/Video.js
@@ -4,11 +4,19 @@ import PropTypes from "prop-types";
 const Video = (props) => {
     const autoPlay = props.autoPlay !== undefined ? props.autoPlay : true;
     const loop = props.loop !== undefined ? props.loop : true;
+    const muted = props.muted !== undefined ? props.muted : true;
+    const width = props.width !== undefined ? props.width : "600px";
     const type = "video/" + props.src.split(".").at(-1);
 
     return (
         <div className="game-video">
-            <video width="600px" autoPlay={autoPlay} loop={loop} controls={props.controls}>
+            <video
+                width={width}
+                autoPlay={autoPlay}
+                loop={loop}
+                controls={props.controls}
+                muted={muted}
+            >
                 <source src={props.src} type={type}/>
             </video>
         </div>
@@ -19,7 +27,9 @@ Video.propTypes = {
     src: PropTypes.string.isRequired,
     autoPlay: PropTypes.bool,
     loop: PropTypes.bool,
-    controls: PropTypes.bool
+    controls: PropTypes.bool,
+    muted: PropTypes.bool,
+    width: PropTypes.string
 };
 
 export default Video;
